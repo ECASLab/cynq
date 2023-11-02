47cd9d7e7108c8c1dad4cb9193d34b7357b6a3e1
Diego Avila <dandida95@gmail.com>
2023-11-02 12:17:24 -0600

    Add header files with basic interfaces
    This commit adds the header files with the
    basic interfaces for specialisation of the hardware,
    accelerators and memory-related structures

---
+++ include/cynq/accelerator.hpp
@@ -0,0 +1,13 @@
+/*
+ * See LICENSE for more information about licensing
+ *
+ * Copyright 2023
+ * Author: Luis G. Leon-Vega <luis.leon@ieee.org>
+ *         Diego Arturo Avila Torres <diego.avila@uned.cr>
+ *
+ */
+#pragma once
+
+namespace cynq {
+class IAccelerator {}
+}  // namespace cynq
