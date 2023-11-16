464ab90c33e3f694d13dec25fc02b8acc4f44216
Luis Leon <lleon95@gmail.com>
2023-11-16 14:15:36 +0100

    Complete the memory addressing support
    It adds the devptr and hostptr determination

--- include/cynq/status.hpp
+++ include/cynq/status.hpp
@@ -28,6 +28,7 @@ struct Status {
     CONFIGURATION_ERROR, /** Configuration error*/
     REGISTER_IO_ERROR,   /** Register MMIO error */
     NOT_IMPLEMENTED,     /** Not implemented error */
+    MEMBER_ABSENT,       /** Missing member */
   };
 
   int code;        /** Code of the error */
