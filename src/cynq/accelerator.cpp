adbaa5207fb8e0d028f632f100bdc661db4ddc4d
Luis Leon <lleon95@gmail.com>
2024-05-09 12:52:16 +0000

    Add asynchronous API to the IAccelerator
    This implements interface-level wrappers for the asynchronous
    execution such that they don't need to be implemented in the
    class implementations

--- src/cynq/accelerator.cpp
+++ src/cynq/accelerator.cpp
@@ -7,6 +7,7 @@
  *
  */
 #include <cynq/accelerator.hpp>
+#include <cynq/execution-graph.hpp>
 #include <cynq/mmio/accelerator.hpp>
 #include <cynq/xrt/accelerator.hpp>
 #include <memory>
@@ -23,7 +24,7 @@ std::shared_ptr<IAccelerator> IAccelerator::Create(IAccelerator::Type impl,
 }
 
 std::shared_ptr<IAccelerator> IAccelerator::Create(
-    IAccelerator::Type impl, const std::string& addr,
+    IAccelerator::Type impl, const std::string &addr,
     const std::shared_ptr<HardwareParameters> params) {
   switch (impl) {
     case IAccelerator::Type::XRT:
@@ -32,4 +33,102 @@ std::shared_ptr<IAccelerator> IAccelerator::Create(
       return nullptr;
   }
 }
+
+/*
+   -- Overloaded operations with fixed implementation --
+   These functions are agnostic and independent from the
+   platform but still require some implementable componets
+*/
+
+Status IAccelerator::Start(std::shared_ptr<IExecutionGraph> graph,
+                           const StartMode mode) {
+  Status st{};
+
+  /* Check the stream */
+  if (!graph) {
+    return this->Start(mode);
+  }
+
+  /* Functor to execute  */
+  IExecutionGraph::Function func = [&, mode]() -> Status {
+    return this->Start(mode);
+  };
+
+  /* Add function */
+  st.retval = graph->Add(func);
+  return st;
+}
+
+Status IAccelerator::Stop(std::shared_ptr<IExecutionGraph> graph) {
+  Status st{};
+
+  /* Check the stream */
+  if (!graph) {
+    return this->Stop();
+  }
+
+  /* Functor to execute  */
+  IExecutionGraph::Function func = [&]() -> Status { return this->Stop(); };
+
+  /* Add function */
+  st.retval = graph->Add(func);
+  return st;
+}
+
+Status IAccelerator::Sync(std::shared_ptr<IExecutionGraph> graph) {
+  Status st{};
+
+  /* Check the stream */
+  if (!graph) {
+    return this->Sync();
+  }
+
+  /* Functor to execute  */
+  IExecutionGraph::Function func = [&]() -> Status { return this->Sync(); };
+
+  /* Add function */
+  st.retval = graph->Add(func);
+  return st;
+}
+
+Status IAccelerator::WriteRegister(std::shared_ptr<IExecutionGraph> graph,
+                                   const uint64_t address, const uint8_t *data,
+                                   const size_t size) {
+  Status st{};
+
+  /* Check the stream */
+  if (!graph) {
+    return this->WriteRegister(address, data, size);
+  }
+
+  /* Functor to execute  */
+  IExecutionGraph::Function func = [&, address, data, size]() -> Status {
+    return this->WriteRegister(address, data, size);
+  };
+
+  /* Add function */
+  st.retval = graph->Add(func);
+  return st;
+}
+
+Status IAccelerator::ReadRegister(std::shared_ptr<IExecutionGraph> graph,
+                                  const uint64_t address, uint8_t *data,
+                                  const size_t size) {
+  Status st{};
+
+  /* Check the stream */
+  if (!graph) {
+    return this->ReadRegister(address, data, size);
+  }
+
+  /* Functor to execute  */
+  IExecutionGraph::Function func = [&, address, data, size]() -> Status {
+    return this->ReadRegister(address, data, size);
+  };
+
+  /* Add function */
+  st.retval = graph->Add(func);
+  return st;
+}
+
 }  // namespace cynq
