9db030a87eeab5ebdd7ee81f180751f0b8fc8b2b
Diego Avila <dandida95@gmail.com>
2025-09-02 13:41:02 +0000

    Basic rvv example
    adds files necessary for risc-v experimentation

--- examples/zynq-mpsoc/riscv_experiment.cpp
+++ examples/zynq-mpsoc/riscv_experiment.cpp
@@ -14,7 +14,7 @@
 static constexpr uint32_t HP0_DDR_LOW = 0x0;
 static constexpr uint32_t HP0_DDR_HIGH = 0x80000000;
 static constexpr uint32_t HP0_QSPI = 0xC0000000;
-static constexpr uint32_t AXI_CONTROL_REG = 2684878848;
+static constexpr uint32_t AXI_CONTROL_REG = 2684354560;
 
 #define XRV32I_NPP_IP_CONTROL_ADDR_AP_CTRL 0x00
 #define XRV32I_NPP_IP_CONTROL_ADDR_GIE 0x04
